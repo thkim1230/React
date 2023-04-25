a80a6b3928aa61e48691497dc501971c75d60076
thkim <thkim1347@gmail.com>
2023-04-25 14:12:39 +0900

    4/25

---
+++ src/api/Axios.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+// 서버 주소
+const KH_DOMAIN = "http://localhost:8111";
+const FRONTEND_DOMAIN = "http://localhost:3000";
+
+
+const AxiosApi = {
+    restaurantInfo: async (restaurantId) => {
+      return await axios.get(
+        `${KH_DOMAIN}/restaurant/info/${restaurantId}`,
+        {
+          headers: {
+            "Access-Control-Allow-Origin": FRONTEND_DOMAIN,
+          },
+        }
+      );
+    },
+    
+  };
+  
+
+
+export default AxiosApi;
